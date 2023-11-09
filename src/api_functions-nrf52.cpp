679822b5e321acb49c926f73ac0f4cbb0a70f8f4
Andreas Schneider <andreas.schneider@mfa-informatik.ch>
2023-11-09 18:17:31 +0000

    Moved variables to api_functions.h

--- src/api_functions-nrf52.cpp
+++ src/api_functions-nrf52.cpp
@@ -2,33 +2,8 @@
 
 #include "WisBlock-API-V2.h"
 
-// Define alternate pdMS_TO_TICKS that casts uint64_t for long intervals due to limitation in nrf52840 BSP
-#define mypdMS_TO_TICKS(xTimeInMs) ((TickType_t)(((uint64_t)(xTimeInMs)*configTICK_RATE_HZ) / 1000))
+#include "api_functions.h"
 
-#ifdef SW_VERSION_1
-uint16_t g_sw_ver_1 = SW_VERSION_1; // major version increase on API change / not backwards compatible
-#else
-uint16_t g_sw_ver_1 = 1; // major version increase on API change / not backwards compatible
-#endif
-#ifdef SW_VERSION_2
-uint16_t g_sw_ver_2 = SW_VERSION_2; // minor version increase on API change / backward compatible
-#else
-uint16_t g_sw_ver_2 = 0; // minor version increase on API change / backward compatible
-#endif
-#ifdef SW_VERSION_3
-uint16_t g_sw_ver_3 = SW_VERSION_3; // patch version increase on bugfix, no affect on API
-#else
-uint16_t g_sw_ver_3 = 0; // patch version increase on bugfix, no affect on API
-#endif
-
-String g_device_pid = "Custom";
-String g_custom_fw_ver = "unset";
-
-/** Buffer for device alias (WisToolBox) */
-char g_alias[16] = {0x00};
-
-/** Flag if device alias has been set */
-bool g_has_alias = false;
 
 /**
  * @brief Set device alias
